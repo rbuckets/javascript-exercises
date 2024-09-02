b5b8bb136388ef232ea1b87854c4805a50184976
rbuckets <wangrayray@gmail.com>
2024-09-02 14:38:28 -0700

    finished exercise 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removedElements) {
+  let res = []
+  let removedElementsArray = Array.from(removedElements)
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!removedElementsArray.includes(arr[i])) {
+      res.push(arr[i])
+    }
+  }
+  return res
 };
 
 // Do not edit below this line
