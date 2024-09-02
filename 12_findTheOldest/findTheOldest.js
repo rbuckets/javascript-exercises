a06e6e959739cd56dd02c390316706a5a6812123
rbuckets <wangrayray@gmail.com>
2024-09-02 15:26:27 -0700

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldestPerson = null;
+  let oldestAge = 0;
 
+  for (let person of people) {
+    let yearToSubtract;
+    if (person.yearOfDeath == null) {
+      yearToSubtract = 2024;
+    } else {
+      yearToSubtract = person.yearOfDeath;
+    }
+    let age = yearToSubtract - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldestPerson = person;
+      oldestAge = age;
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
