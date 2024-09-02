479ae0f5326db2df7cb06463ca7f56944a8fcf70
rbuckets <wangrayray@gmail.com>
2024-09-02 15:19:51 -0700

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  num = parseInt(num);
 
+  if (num < 0) {
+    return "OOPS";
+  }
+  if (num == 0) {
+    return 0;
+  }
+  if (num <= 2) {
+    return 1;
+  }
+
+  let first = 1;
+  let second = 1;
+  for (let i = 3; i <= num; i++) {
+    let temp = second;
+    second = first + second;
+    first = temp;
+  }
+
+  return second;
 };
 
 // Do not edit below this line
