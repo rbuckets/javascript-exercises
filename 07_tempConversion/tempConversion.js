40726814a02d2355c627a6148e0f8f9c37d3bab9
rbuckets <wangrayray@gmail.com>
2024-09-02 14:59:01 -0700

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let resUnrounded = (temperature - 32) / (9 / 5);
+  return Math.round(resUnrounded * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let resUnrounded = temperature * (9 / 5) + 32;
+  return Math.round(resUnrounded * 10) / 10;
 };
 
 // Do not edit below this line
