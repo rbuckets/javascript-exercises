014b38ff0a1caa0e2ebc1a8fdcc63571aad6133e
rbuckets <wangrayray@gmail.com>
2024-09-02 14:49:18 -0700

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  // checking for input errors
+  if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
+  let res = 0;
 
+  // swapping if start > end
+  if (start > end) {
+    let temp = end;
+    end = start;
+    start = temp;
+  }
+
+  // adding
+  for (let i = start; i <= end; i++) {
+    res += i;
+  }
+  return res;
 };
 
 // Do not edit below this line
