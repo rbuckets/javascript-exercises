dfc8ab87f3e84aed7ec5117d097b3b189d601f8a
rbuckets <wangrayray@gmail.com>
2024-09-02 15:13:20 -0700

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const reverseString = function(string) {
+  let res = ""
+  for (let i = string.length - 1; i >= 0; i--) {
+    res += string[i]
+  }
+  return res
+};
 
+const palindromes = function (string) {
+  let strippedString = string.toLowerCase().replace(/[.,\s!]/g, '');
+  return reverseString(strippedString) === strippedString;
 };
 
 // Do not edit below this line
