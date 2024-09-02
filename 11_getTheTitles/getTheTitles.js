1c1c639ac80556684065075945924a2d6942b7f7
rbuckets <wangrayray@gmail.com>
2024-09-02 15:22:14 -0700

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  res = [];
+  for (let book of books) {
+    res.push(book.title);
+  }
+  return res;
 };
 
 // Do not edit below this line
