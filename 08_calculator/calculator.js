674eafb38f4662c7deb9d50562c0a98c320492fd
rbuckets <wangrayray@gmail.com>
2024-09-02 15:05:54 -0700

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+  return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let res = 0;
+  for (let i = 0; i < nums.length; i++) {
+    res += nums[i];
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let res = 1;
+  for (let i = 0; i < nums.length; i++) {
+    res *= nums[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+  for (let i = 1; i <= num; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
